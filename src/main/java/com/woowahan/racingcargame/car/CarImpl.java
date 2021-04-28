cc910c2a1de515e98831ebb2e98a9f79ed694b46
YoonBing9 <akass191@gmail.com>
2021-04-28 18:36:43 +0900

    feat(Car): 0~9까지의 랜덤 숫자 발생 기능

--- src/main/java/com/woowahan/racingcargame/car/CarImpl.java
+++ src/main/java/com/woowahan/racingcargame/car/CarImpl.java
@@ -2,9 +2,12 @@ package com.woowahan.racingcargame.car;
 
 import com.woowahan.racingcargame.message.ExceptionMessage;
 
-public class CarImpl implements Car{
+import java.util.Random;
+
+public class CarImpl implements Car {
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
+    private static final int RANDOM_NUMBER_RANGE = 9; // 0~9
     private final String name;
 
     public CarImpl(String name) {
@@ -13,7 +16,7 @@ public class CarImpl implements Car{
     }
 
     private void validationNameLength(String name) {
-        if(isValidLength(name)) {
+        if (isValidLength(name)) {
             throw new IllegalArgumentException(ExceptionMessage.CAR_NAME_LENGTH);
         }
     }
@@ -22,8 +25,13 @@ public class CarImpl implements Car{
         return MIN_NAME_LENGTH > name.length() || name.length() > MAX_NAME_LENGTH;
     }
 
+    private int createRandomNumber() {
+        return new Random().nextInt(RANDOM_NUMBER_RANGE);
+    }
+
     @Override
-    public void putPedal() {}
+    public void putPedal() {
+    }
 
     @Override
     public String getName() {
--- src/test/java/com/woowahan/racingcargame/car/CarImplTest.java
+++ src/test/java/com/woowahan/racingcargame/car/CarImplTest.java
@@ -1,9 +1,13 @@
 package com.woowahan.racingcargame.car;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -32,6 +36,29 @@ class CarImplTest {
             //when
             Car car = new CarImpl(carName);
         });
+    }
+
+    @Test
+    @DisplayName("랜덤 숫자 범위 검증")
+    void validate_random_number_range() {
+        try {
+            //given
+            Car car = new CarImpl("test");
+            Method method = car.getClass().getDeclaredMethod("createRandomNumber");
+            method.setAccessible(true);
+
+            //when
+            int randomNumber = (int) method.invoke(car);
+
+            //then
+            assertThat(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}).contains(randomNumber);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
 }
